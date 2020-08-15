3b02482805d5b1c6d1081bf52696cffc9241f2aa
juandimarco <68037364+juandimarco@users.noreply.github.com>
2020-08-15 19:34:38 -0300

    Entrega 16/8/2020
    Agregar lista de productos
    Redireccionar del index al login
    Que funcione el login(sin autentificar)

--- js/login.js
+++ js/login.js
@@ -1,6 +1,20 @@
+function manejadorDeEvento(evento) {
+    evento.preventDefault();
+    sessionStorage.setItem("loged", "true");
+    window.location.href = "index.html"
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("form").addEventListener("submit", manejadorDeEvento)
 });
+
+
+/*function login() {
+    if (localStorage.getItem("email").value !== "" & inputPassword !== "") {
+    window.location.assign("index.html");
+    }
+}*/
